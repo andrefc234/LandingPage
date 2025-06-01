853a65f7246afc2ab46760221b22c1b9218a518a
andrefc234 <49660774+andrefc234@users.noreply.github.com>
2025-06-01 12:52:33 -0600

    Create Hero.jsx

---
+++ src/components/Hero.jsx
@@ -0,0 +1,25 @@
+import { Box, Heading, Text, Image } from "@chakra-ui/react";
+
+export default function Hero() {
+  return (
+    <Box
+      bgGradient="linear(to-br, brand.primary, brand.secondary)"
+      p={10}
+      textAlign="center"
+    >
+      <Image
+        src="/images/portada.jpg"
+        alt="Portada del artista"
+        mx="auto"
+        borderRadius="lg"
+        maxW="300px"
+      />
+      <Heading mt={6} color="white">
+        Nombre del Artista
+      </Heading>
+      <Text fontSize="xl" mt={2} color="whiteAlpha.800">
+        Sonido urbano, puro estilo.
+      </Text>
+    </Box>
+  );
+}
