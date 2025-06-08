8bd8270a973df1ebf117cf0b702150ad9e803008
andrefc234 <49660774+andrefc234@users.noreply.github.com>
2025-06-07 22:05:38 -0600

    changes

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,16 +1,16 @@
 
 import Navbar from "../components/Navbar";
+
 import Head from 'next/head';
-import Hero from '@/components/Hero';
-import LatestRelease from '@/components/LatestRelease';
-import MusicPlayer from '@/components/MusicPlayer';
-import TourDates from '@/components/TourDates';
-import Merch from '@/components/Merch';
-import Gallery from '@/components/Gallery';
-import SocialLinks from '@/components/SocialLinks';
-import Testimonials from '@/components/Testimonials';
-import Newsletter from '@/components/Newsletter';
-import Footer from '@/components/Footer';
+import Hero from '../components/Hero';
+import LatestRelease from '../components/LatestRelease';
+import MusicPlayer from '../components/MusicPlayer';
+
+import Merch from '../components/Merch';
+import Gallery from '../components/Gallery';
+import SocialMedia from '../components/SocialLinks';
+
+import Footer from '../components/Footer';
 
 export default function Home() {
   return (
@@ -24,12 +24,11 @@ export default function Home() {
       <Hero />
       <LatestRelease />
       <MusicPlayer />
-      <TourDates />
+    
       <Merch />
       <Gallery />
-      <SocialLinks />
-      <Testimonials />
-      <Newsletter />
+      <SocialMedia />
+   
       <Footer />
     </>
   );
