4f632db640f32ce7e6d978f507ccf0b79bb45335
andrefc234 <49660774+andrefc234@users.noreply.github.com>
2025-06-07 22:12:42 -0600

    add react slick

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,5 +1,7 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import theme from "../theme";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 function MyApp({ Component, pageProps }) {
   return (
